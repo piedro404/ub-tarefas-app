843e96f921d14c499285f1fd4a8d98f4f5867022
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-05-15 18:41:32 -0300

    feat: :seedling: Fetch Task Api

--- src/pages/Tasks/index.tsx
+++ src/pages/Tasks/index.tsx
@@ -1,18 +1,36 @@
 import React, { useEffect, useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, RefreshControl } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
 import styles from "./styles";
 
+import { apiUrl } from "@scripts/apiUrl";
+
+interface Task {
+  date: string;
+  day_week: string;
+  matter: string;
+  task_name: string;
+  time_limit: string;
+  url_task: string;
+}
+
+interface TaskDetails {
+  amount_task: number;
+  description: string;
+  find_task: boolean;
+  list_tasks: Task[];
+}
+
 interface Storage {
   login?: string | null | undefined;
   password?: string | null | undefined;
-  tasks?: string | null | undefined;
+  tasksDetails?: TaskDetails | null | undefined;
 }
 
 export default function Tasks() {
-  const [tasksDetails, setTasksDetails] = useState<Storage>({});
+  const [storage, setStorage] = useState<Storage>({});
 
   useEffect(() => {
     getData();
@@ -20,19 +38,46 @@ export default function Tasks() {
 
   async function getData() {
     try {
-      setTasksDetails({
+      const tasks = await AsyncStorage.getItem("tasks");
+      setStorage({
         login: await AsyncStorage.getItem("login"),
         password: await AsyncStorage.getItem("password"),
-        tasks: await AsyncStorage.getItem("tasks"),
+        tasksDetails: tasks ? JSON.parse(tasks) : {},
       });
+      await getSearchTasks();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function getSearchTasks() {
+    try {
+      const { login, password } = storage;
+      const response = await axios.post(apiUrl({ params: "/ub/task" }), {
+        login: login,
+        password: password,
+      });
+      if (response.status === 200 && response.data.status === true) {
+        setStorage((prevState) => ({
+          ...prevState,
+          tasksDetails: response.data.tasks,
+        }));
+      }
     } catch (error) {
       console.log(error);
     }
   }
 
   return (
-    <View>
-      <Text>Tarefas....</Text>
+    <View style={styles.container}>
+      <View style={styles.infos}></View>
+      <View style={styles.tasks}>
+        {storage.tasksDetails &&
+          storage.tasksDetails.find_task &&
+          storage.tasksDetails.list_tasks.map((task, index) => {
+            return <Text key={index}>{task.task_name}</Text>;
+          })}
+      </View>
     </View>
   );
 }
